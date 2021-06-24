6bcaddc08d4bebb84cd1a707e7860871ef2f3630
Bulbul Niaz <83117381+bulbul85@users.noreply.github.com>
2021-06-24 11:55:08 -0700

    Add files via upload

---
+++ conditionals/app1.js/app.js
@@ -0,0 +1,14 @@
+/*
+Consider the situation in which you are required to check if a number is 1 or 0.
+If it’s 1 you need to print true. Otherwise, you should print false.
+*/
+
+let number = 1;
+
+if (number == 1){
+console.log("True");
+}
+
+else{
+console.log("False");
+}
---
+++ conditionals/app2.js/app.js
@@ -0,0 +1,14 @@
+/*
+You are given two numbers, num1 = 4 and num2 = 7.
+Add both and return true if both are equal to 11. Otherwise, return false, if they don’t equal 11.
+*/
+
+let num1 = 4, num2 = 7;
+let sum = num1 + num2;
+
+if (sum == 11){
+console.log("True");
+}
+else {
+console.log("False");
+}
